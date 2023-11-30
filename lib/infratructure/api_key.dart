303dd51b787138cb8e3390b11347ba635c5561d0
ishaqsah <ishaqsah123@gmail.com>
2023-11-30 16:36:51 +0530

    git ignore

---
+++ lib/infratructure/api_key.dart
@@ -0,0 +1,4 @@
+const apikey = 'c968f0908e165f5da4969f08e5900e6e';
+
+final readaccesstoken =
+'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTY4ZjA5MDhlMTY1ZjVkYTQ5NjlmMDhlNTkwMGU2ZSIsInN1YiI6IjY0Yzc1NzU1NjNlNmZiMDBjNDA5ZDM3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I_UJnROO0ypvRvl7uyBKUhhgd_aVkgDYlRD6OCd3ISI';
--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,8 @@ import 'package:netflixapp/presentation/screen_mainpage/widgets/Screen_Main.dart
 void main() {
   runApp(const MyApp());
 }
- final String apitoken = 'c968f0908e165f5da4969f08e5900e6e';
-  final readaccesstoken =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTY4ZjA5MDhlMTY1ZjVkYTQ5NjlmMDhlNTkwMGU2ZSIsInN1YiI6IjY0Yzc1NzU1NjNlNmZiMDBjNDA5ZDM3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I_UJnROO0ypvRvl7uyBKUhhgd_aVkgDYlRD6OCd3ISI';
 
+  
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
